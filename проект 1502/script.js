f1c761a3265f9435713d08a55b6025a1fac6f0b0
rmd09 <rmd09@yandex.ru>
2024-01-11 00:46:10 +0300

    Помидор умеет выскакивать и исчезать

--- проект 1502/script.js
+++ проект 1502/script.js
@@ -1,5 +1,8 @@
 const inputbox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
+const button = document.getElementById('pomidor-button');
+const timer = document.getElementById('timer');
+let isPomidorShowing = false;
 
 function addTask() {
     if (inputbox.value === '') {
@@ -33,3 +36,25 @@ function showtask() {
     listcontainer.innerHTML = localStorage.getItem("data");
 }
 showtask();
+
+//
+const showPomidor = (e) => {
+    if (isPomidorShowing)
+    {
+        timer.style.display = "none";
+        button.style.background = '#555';
+        button.style.border = 'none';
+        button.style.color = '#fff';
+        isPomidorShowing = !isPomidorShowing;
+    }
+    else
+    {
+        timer.style.display = "flex";
+        button.style.background = 'none';
+        button.style.border = '#000 solid 1px';
+        button.style.color = '#000';
+        isPomidorShowing = !isPomidorShowing;
+        //какой-то метод
+    }
+}
+button.addEventListener('click', showPomidor);
